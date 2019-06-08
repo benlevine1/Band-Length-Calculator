d07475a2726cd1a476d9a813c65cf73dc5dd6d6e
benlevine1 <benlevine1@gmail.com>
2019-06-07 20:49:58 -0700

    basic app working, need to add styling

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-$(document).ready()
+$(document).ready(addClickHandlers)
 
+function addClickHandlers(){
+  $('#calculate').click(calculateLength);
+}
+
+const calculateLength = () => {
+  let initialLength = $('#length').val();
+  let percentage = $('.mySelect option:selected').val();
+  const result = (initialLength * 2 / percentage).toFixed(3);
+  $('#result').val(`${result} in.`);
+}
